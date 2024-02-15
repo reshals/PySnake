7b1656c871cf44398c72215190bcc86ec5f263b5
reshals <36654357+reshals@users.noreply.github.com>
2024-02-15 16:08:11 -0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -2,7 +2,13 @@ import curses
 import time
 
 def main(screen):
+	curses.curs_set(0)
 	time.sleep(1)
+	snake = [(0, i) for i in reversed(range(20))]
+	screen.addstr(*snake[0], '@')
+	for segment in snake[1:]:
+   		screen.addstr(*segment, '*')
+	#print(snake)
 
 if __name__ == '__main__':
 	curses.wrapper(main)
